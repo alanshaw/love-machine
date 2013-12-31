e32e773b273c1b82730ef748aca474385a6727df
Alan Shaw <alan@freestyle-developments.co.uk>
2013-12-31 09:50:29 +0000

    Gratuitous spin and fade in new tweet

--- main.js
+++ main.js
@@ -15,6 +15,10 @@
     }
   }
 
+  function spin () {
+    heart.className = heart.className ? "" : "spin"
+  }
+
   var tweets = [
     "@dave: Northern Ontario man kills 60-pound lynx with bare hands to save his dogs: http://bit.ly/1hQ7XzO  pic.twitter.com/myRP8pC8Ef",
     "@bob_qillin: Grab a bargain! Get the HTC One & save £170. Now £49.99 upfront at £27 p/m. Ends 8th Jan. #lizzyalanwedding",
@@ -34,9 +38,26 @@
       tIndex++
     }
 
-    heart.innerHTML = heart.innerHTML + " <span>" + tweet + "</span>"
+    spin()
 
-    reflow()
+    // Add the text exactly half way through the spin
+    setTimeout(function () {
+
+      // Buggy shape-inside implementation doesn't work if we append a node (2013-12-31)
+      heart.innerHTML = heart.innerHTML + " <span class=\"new\">" + tweet + "</span>"
+
+      reflow()
+
+      // Allow CSS to transition the tweet in the next tick
+      setTimeout(function () {
+        var tweets = document.getElementsByClassName("new")
+        for (var i = 0; i < tweets.length; ++i) {
+          tweets[i].className = ""
+        }
+      }, 0)
+
+    }, 400)
+
   })
 
 }()
